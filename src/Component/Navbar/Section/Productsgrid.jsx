a9644d296877a1dc9a0939cf3fb678a4ffff8f61
Sajib <sajibdashtrg@gmail.com>
2025-10-22 01:23:29 +0600

    Implement Productsgrid component with AOS animations and product display

--- src/Component/Navbar/Section/Productsgrid.jsx
+++ src/Component/Navbar/Section/Productsgrid.jsx
@@ -1,7 +1,63 @@
-import React from 'react'
+import { MdOutlineRemoveRedEye, MdAddShoppingCart } from "react-icons/md";
+import { FaRegHeart, FaStar } from "react-icons/fa6";
+import AOS from "aos";
+import "aos/dist/aos.css";
+import React, { useEffect } from "react";
+import { products } from "../../../export";
 
 export const Productsgrid = () => {
+  var settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1, // একবারে ১টা ছবি দেখাও
+    slidesToScroll: 1,
+    arrows: false, // এখানে সঠিক key
+    autoplay: true,
+    autoplaySpeed: 3000,
+  };
+  useEffect(() => {
+    AOS.init({
+      offset: 100,
+      duration: 500,
+      easing: "ease-in-out", // এখানে 'easing' বানান ঠিক করলাম
+    });
+    AOS.refresh();
+  }, []);
+
   return (
-    <div>Productsgrid</div>
-  )
-}
+    <div
+      id="products"
+      className="w-full lg:px-20 px-5 py-[80px] bg-gray-100 flex flex-col justify-center gap-4"
+    >
+      <h1
+        data-aos="zoom-in"
+        data-aos-delay="100"
+        className="text-[#502ec3] text-xl text-center font-semibold "
+      >
+        Browse Collection
+      </h1>
+      <h1
+        data-aos="zoom-in"
+        data-aos-delay="200"
+        className="text-black font-semibold text-[42px] leadding-[50px] text-center"
+      >
+        Trending Products
+      </h1>
+      <div
+        data-aos="zoom-in"
+        data-aos-delay="300"
+        className="w-full grid lg:grid-cols-4 grid-cols-1 justify-center items-center gap-10 mt-10 "
+      >
+        {products.map((item, index) => (
+          <div
+            id="Product-box"
+            key={index}
+            className="flex flex-col justify-center items-center gap-2 bg-white p-4 rounded-lg cursor-pointer relative"
+          ></div>
+          
+        ))}
+      </div>
+    </div>
+  );
+};
