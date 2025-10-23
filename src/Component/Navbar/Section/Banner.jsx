34c05de857da6c5ff2876cb8c337220aafb02218
Sajib <sajibdashtrg@gmail.com>
2025-10-23 12:07:10 +0600

    Implement Banner component with AOS animations, improved layout, and background image

--- src/Component/Navbar/Section/Banner.jsx
+++ src/Component/Navbar/Section/Banner.jsx
@@ -1,7 +1,52 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
+import deal from "../../../assets/deal-bg.jpg";
 
 export const Banner = () => {
+  var settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1, // একবারে ১টা ছবি দেখাও
+    slidesToScroll: 1,
+    arrows: false, // এখানে সঠিক key
+    autoplay: true,
+    autoplaySpeed: 3000,
+  };
+
+  useEffect(() => {
+    AOS.init({
+      offset: 100,
+      duration: 500,
+      easing: "ease-in-out", // এখানে 'easing' বানান ঠিক করলাম
+    });
+    AOS.refresh();
+  }, []);
+
   return (
-    <div>Banner</div>
-  )
-}
+    <div className="w-full lg:px-20 px-5 py-[80px]">
+      <div
+        data-aos="zoom-in"
+        data-aos-delay="100"
+        className="w-full h-[300px] rounded-lg bg-cover bg-center flex flex-col justify-center items-center gap-3"
+        style={{ backgroundImage: `url(${deal})` }}
+      >
+        <h1 className="text-[#f5a60d] text-xl font-semibold">
+          Every day shopping
+        </h1>
+        <h1 className="text-white font-bold text-[42px] leading-[50px] text-center">
+          Deal of the Day
+        </h1>
+        {/* Shop now button */}
+        <button
+        data-aos="zoom-in"
+        data-aos-delay="400"
+        className="self-center bg-[#f5a60d] hover:bg-white text-white hover:text-black font-semibold px-10 py-3 rounded-lg mt-10 transition duration-300"
+      >
+        SHOPE NOW
+      </button>
+      </div>
+    </div>
+  );
+};
